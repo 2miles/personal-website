2b2e86d80966ee7fbbba0554df61e4d9de5575be
miles <miles4599@gmail.com>
2022-08-08 23:04:38 -0700

    added carousel for projects

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-var docWidth = document.documentElement.offsetWidth;
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
 
-[].forEach.call(document.querySelectorAll("*"), function (el) {
-  if (el.offsetWidth > docWidth) {
-    console.log(el);
-  }
+nextButton.addEventListener("click", (event) => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
 });
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+
+console.log("fuck you");
