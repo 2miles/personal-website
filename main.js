bbac6ca2e33f37f3fdf61236662600b5e78a6793
miles <miles4599@gmail.com>
2022-08-10 16:23:52 -0700

    updated main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const slidesContainer = document.getElementById("slides-container");
+const slides = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+const prev = document.getElementById("slide-arrow-prev");
+const next = document.getElementById("slide-arrow-next");
 
-nextButton.addEventListener("click", (event) => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
+next.addEventListener("click", () => {
+  const width = slide.clientWidth;
+  slides.scrollLeft += width;
 });
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
+prev.addEventListener("click", () => {
+  const width = slide.clientWidth;
+  slides.scrollLeft -= width;
 });
